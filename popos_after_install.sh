5b8a6084094fe09f802cd643e10c09691e3015fb
eroot <eroot@pop-os.localdomain>
2023-10-29 14:46:13 +0300

    Удалён лишний софт

--- popos_after_install.sh
+++ popos_after_install.sh
@@ -3,25 +3,12 @@
 sudo add-apt-repository universe
 sudo apt update
 sudo apt full-upgrade -y
-#add VirtualBox repo
-sudo bash -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian jammy contrib" >> /etc/apt/sources.list'
-wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmor --es --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
-#install and enable ssh
-sudo apt install openssh-server -y
-sudo systemctl start sshd
 #repo for wine
 sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/kinetic/winehq-kinetic.sources
 #client for eve-ng
 sudo add-apt-repository ppa:smartfinn/eve-ng-integration
-#Ключ для Sublime-text
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-#repo for Ansible
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-#curl and ansible
-sudo apt install curl ansible libu2f-udev -y
 #repo for syncthing
 sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
@@ -48,7 +35,7 @@ sudo apt install wireguard -y
 sudo apt install --install-recommends winehq-stable -y
 wine winecfg
 #install soft from repos
-sudo apt install aptitude virtualbox-7.0 gnome-tweaks gnome-shell-extension-manager ubuntu-restricted-extras nmap wireshark putty gdebi keepassxc vlc audacity gimp obs-studio zeal eve-ng-integration -y
+sudo apt install libu2f-udev curl aptitude virtualbox-7.0 gnome-tweaks gnome-shell-extension-manager ubuntu-restricted-extras nmap wireshark putty gdebi keepassxc vlc audacity gimp obs-studio zeal eve-ng-integration -y
 sudo apt install winetricks sublime-text syncthing librewolf darktable filezilla docker openjdk-17-jre-headless default-jre steam-installer python2 python3-pip python3-venv -y
 #install kali repos
 sudo sh -c "echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list" -y
@@ -74,5 +61,5 @@ mv zsh-syntax-highlighting ~/.oh-my-zsh/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions
 mv zsh-autosuggestions ~/.oh-my-zsh/custom/plugins
 #Затем добавьте плагин в список плагинов в файле ~/.zshrc: plugins=(git docker gcloud terraform vagrant zsh-autosuggestions)
-zsh
-source ~/.zshrc
+# zsh
+# source ~/.zshrc
