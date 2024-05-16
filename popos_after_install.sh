682cec668f66d861f8e59dd590afda283d8859d9
Magnus Root <magnus-root@tzeentch.ru>
2024-05-16 10:03:42 +0300

    Добавлен драйвер для Альфы

--- popos_after_install.sh
+++ popos_after_install.sh
@@ -1,37 +1,37 @@
-#! /bin/bash
+#!/usr/bin/env bash
 sudo apt update
 sudo apt full-upgrade -y
-#add universal repo
+# Универсальная репа
 sudo add-apt-repository universe -y
-#repo for wine
+# Репа для wine
 sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/kinetic/winehq-kinetic.sources
 #repo for sublime-text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-#client for eve-ng
+# Репа для eve-ng
 sudo add-apt-repository ppa:smartfinn/eve-ng-integration <<EOF
 EOF
-#repo for yubiko
+# Репа для yubiko
 sudo add-apt-repository ppa:yubico/stable <<EOF
 EOF
-#repo for HP
+# Репа для HP
 sudo add-apt-repository ppa:kelebek333/hplip -y
-# Add Docker's official GPG key:
+# Добавляем GPG ключ для Docker
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-# Add the repository to Apt sources:
+# Добавлем репы для Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#repo for syncthing
+# Репа для syncthing
 sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
-#repo for librewolf
+# Репа для librewolf
 sudo apt install -y wget gnupg lsb-release apt-transport-https
 distro=$(if echo " una vanessa focal jammy bullseye vera uma" | grep -q " $(lsb_release -sc) "; then echo $(lsb_release -sc); else echo focal; fi)
 wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
@@ -43,59 +43,56 @@ Components: main
 Architectures: amd64
 Signed-By: /usr/share/keyrings/librewolf.gpg
 EOF
-#update and upgrade with new repos
+# Обновление из новых реп
 sudo apt update
 sudo apt dist-upgrade -y
-#install wireguard
-sudo apt install wireguard -y
-#Создать конфиг для wg в файле - sudo nano /etc/wireguard/wg0.conf
-#Запуск - sudo wg-quick up wg0
-#install wine
+# Устанавливаем wine
 sudo apt install --install-recommends winehq-stable -y
 wine winecfg
-#install soft from repos
-sudo apt install libu2f-udev curl winbind playonlinux aptitude timeshift solaar gnome-tweaks gnome-shell-extension-manager ubuntu-restricted-extras nmap wireshark putty gdebi keepassxc vlc audacity gimp obs-studio zeal eve-ng-integration -y
+# Устанавливаем софт
+sudo apt install libu2f-udev libelf-dev iw curl wireguard winbind playonlinux aptitude timeshift solaar gnome-tweaks gnome-shell-extension-manager ubuntu-restricted-extras nmap wireshark putty gdebi keepassxc vlc audacity gimp obs-studio zeal eve-ng-integration -y
 sudo apt install htop qbittorrent hplip hplip-gui thunderbird code git audacious winetricks sublime-text syncthing librewolf darktable filezilla openjdk-17-jre-headless default-jre openjdk-17-jre steam-installer python3-pip python3-venv -y
 sudo apt install yubikey-manager yubikey-personalization-gui libpam-yubico libpam-u2f yubikey-manager-qt yubioath-desktop -y
 sudo apt install docker-ce docker-ce-cli docker-compose containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo apt install libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-libav -y
-#install kali repos
+# Создать конфиг для wg
+sudo touch /etc/wireguard/wg0.conf
+# Запуск - sudo wg-quick up wg0
+# Устанавливаем репу kali
 sudo sh -c "echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list" -y
 wget 'https://archive.kali.org/archive-key.asc'
 sudo apt-key add archive-key.asc
 sudo apt update
 sudo sh -c "echo 'Package: *'>/etc/apt/preferences.d/kali.pref; echo 'Pin: release a=kali-rolling'>>/etc/apt/preferences.d/kali.pref; echo 'Pin-Priority: 50'>>/etc/apt/preferences.d/kali.pref" -y
-#update
+# Обновляем
 sudo apt update
-#install kali soft
+# Устанавливаем софт из репы kali
 sudo aptitude install -t kali-rolling wpscan dirsearch metasploit-framework gobuster airgeddon bettercap ettercap-graphical hostapd-wpe bully pixiewps dhcpd asleap hashcat hostapd tshark mdk4 hcxdumptool reaver john crunch beef lighttpd -y
-#Скачиваем и устанавливаем Telegram
+# Скачиваем и устанавливаем Telegram
 mkdir ~/Apps
 cd ~/Apps
 wget https://telegram.org/dl/desktop/linux
 tar -xf linux
 rm linux 
 cd ~/
-#Устанавливаем webapp-manager
+# Устанавливаем webapp-manager
 wget http://packages.linuxmint.com/pool/main/w/webapp-manager/webapp-manager_1.3.4_all.deb
 sudo apt install ./webapp-manager_1.3.4_all.deb -y
 rm webapp-manager_1.3.4_all.deb
-#Устанавливаем Bottles и Portproton
+# Устанавливаем Bottles и Portproton
 flatpak install flathub com.usebottles.bottles -y
 flatpak install flathub portproton -y
-#Скачиваем и утсанавливаем Steam
+# Скачиваем и устанавливаем Steam
 wget https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb
 sudo apt install ./steam.deb -y
 rm ./steam.deb
-#Включаем wayland
+# Включаем wayland
 sudo sed -i 's/WaylandEnable=false/WaylandEnable=true/' /etc/gdm3/custom.conf
-#Скачиваем и устанавливаем obsidian
+# Скачиваем и устанавливаем obsidian
 wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.12/obsidian_1.5.12_amd64.deb
 sudo apt install -y ./obsidian_1.5.12_amd64.deb
 rm ./obsidian_1.5.12_amd64.deb
-#Устанавливаем новую версию Gnome
-sudo apt install gnome-session -y
-#Скачиваем и устанавливаем VirtualBox
+# Скачиваем и устанавливаем VirtualBox
 wget https://download.virtualbox.org/virtualbox/7.0.18/virtualbox-7.0_7.0.18-162988\~Ubuntu\~jammy_amd64.deb
 sudo apt install -y ./virtualbox-7.0_7.0.18-162988\~Ubuntu\~jammy_amd64.deb
 rm ./virtualbox-7.0_7.0.18-162988\~Ubuntu\~jammy_amd64.deb
@@ -103,17 +100,28 @@ sudo gpasswd -a $USER vboxusers
 wget https://download.virtualbox.org/virtualbox/7.0.18/Oracle_VM_VirtualBox_Extension_Pack-7.0.18.vbox-extpack
 sudo vboxmanage extpack install Oracle_VM_VirtualBox_Extension_Pack-7.0.18.vbox-extpack -y
 rm Oracle_VM_VirtualBox_Extension_Pack-7.0.18.vbox-extpack
-#install zsh и oh-my-zsh
+# Ещё раз обновляем, чтобы убрать ошибки установки, если они вдруг были
+sudo apt update
+sudo apt full-upgrade -y
+# Устанавливаем драйвера для RTL8812au
+mkdir -p ~/src
+cd ~/src
+git clone https://github.com/morrownr/8812au-20210820.git
+cd ~/src/8812au-20210820
+sudo ./install-driver.sh
+cd ~
+rm -rf ./src
+# Устанавливаем zsh и oh-my-zsh
 sudo apt install zsh -y 
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-#Изменить шелл по-умолчанию
+# Изменить шелл по-умолчанию
 chsh
-#Ввести /bin/zsh
-#Настройка подсветки
+# Ввести /bin/zsh
+# Настройка подсветки
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 mv zsh-syntax-highlighting ~/.oh-my-zsh/plugins 
 echo "source ~/.oh-my-zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-#Настроика автозаполнения на Zsh
+# Настроика автозаполнения на Zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions
 mv zsh-autosuggestions ~/.oh-my-zsh/custom/plugins
 sed -i 's/plugins=(git)/plugins=(git docker zsh-autosuggestions)/' ~/.zshrc 
