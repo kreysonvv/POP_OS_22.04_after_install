36b33304fae8842d44522a73ec5cab35e400c9c8
Magnus Root <magnus-root@tzeentch.ru>
2024-09-02 10:05:33 +0300

    Добавлено обновление репозиториев

--- popos_after_install.sh
+++ popos_after_install.sh
@@ -64,6 +64,7 @@ wget -qO- https://releases.warp.dev/linux/keys/warp.asc | gpg --dearmor > warpdo
 sudo install -D -o root -g root -m 644 warpdotdev.gpg /etc/apt/keyrings/warpdotdev.gpg
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/warpdotdev.gpg] https://releases.warp.dev/linux/deb stable main" > /etc/apt/sources.list.d/warpdotdev.list'
 rm warpdotdev.gpg
+sudo apt update
 # Устанавливаем софт
 sudo apt install libu2f-udev libelf-dev libuv1=1.43.0-1ubuntu0.1 libnode72 iw wireguard zeal gimp -y
 sudo apt install aptitude timeshift solaar npm swaks warp-terminal -y
